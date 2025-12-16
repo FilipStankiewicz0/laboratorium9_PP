e0066b2986d348d5d0dd88105997be35d86b7ba4
student <student@DESKTOP-5POI41D>
2025-12-16 15:41:28 +0100

    Add project files.

---
+++ laboratorium9_PP/laboratorium9_PP.cpp
@@ -0,0 +1,14 @@
+// laboratorium9_PP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+struct Punkt {
+	int x = punkt.x;
+	int y = punkt.y;
+}
+
+int main() {
+	int punkt;
+
+}
