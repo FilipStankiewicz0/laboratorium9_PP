b4fd98ab80bd134cbcdf1d56cfd68dab73810c3f
FilipStankiewicz0 <04filipstankiewicz@gmail.com>
2025-12-16 16:09:36 +0100

    0

--- laboratorium9_PP/laboratorium9_PP.cpp
+++ laboratorium9_PP/laboratorium9_PP.cpp
@@ -1,14 +1,43 @@
 // laboratorium9_PP.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
-#include <iostream>
+// Zadanie 1
+/* #include <iostream>
 using namespace std;
 struct Punkt {
-	int x = punkt.x;
-	int y = punkt.y;
+	int x, y;
+};
+
+int main() {
+	Punkt p;
+	p.x = 10;
+	p.y = 20;
+	cout << "Wspolrzedne punktu to: (" << p.x << ", " << p.y << ")" << endl;
+}
+*/
+
+// Zadanie 2
+/* #include <iostream>
+using namespace std;
+int pole();
+struct prostokat {
+	int a, b;
+};
+
+int pole(int a, int b) {
+	return a * b;
 }
 
 int main() {
-	int punkt;
-
+	prostokat p;
+	p.a = 10;
+	p.b = 20;
+	cout << " Pole prostokata to: " << pole(p.a, p.b) << endl;
 }
+*/
+
+// Zadanie 3
+#include <iostream>
+using namespace std;
+struct trojkat {
+	
+};
